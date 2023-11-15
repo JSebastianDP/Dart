1dbc5b420b769f743823629c467df4b8b731e743
JSebastianDP <sebastiandiaz28001@gmail.com>
2023-11-15 17:37:57 -0500

    Se añade el ejercicio 2, donde se aprenden nuevos tipos de variables

---
+++ 01-dart-intro/02_variables.dart
@@ -0,0 +1,19 @@
+void main (){
+ final String pokemon = "Ditto";
+  
+  final int vida = 100;
+  final bool isAlive = true;
+  final List <String> abilities = ['impostor'];
+  final sprites = ['Ditto/front.png', 'Ditto/back.png'];
+
+  
+  print("""
+  $pokemon
+  $vida
+  $isAlive
+  $abilities
+  $sprites
+  """);
+  
+  
+}
