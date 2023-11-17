951abe512eb87c22679ab553c746cd4e562051fa
JSebastianDP <sebastiandiaz28001@gmail.com>
2023-11-17 18:48:51 -0500

    Se aprende sobre el tipo de variable dynamic y sus posibles usos

--- 01-dart-intro/02_variables.dart
+++ 01-dart-intro/02_variables.dart
@@ -5,6 +5,13 @@ void main (){
   final bool isAlive = true;
   final List <String> abilities = ['impostor'];
   final sprites = ['Ditto/front.png', 'Ditto/back.png'];
+  
+  //Tipo de variable "dynamic", es un tipo de variable que puede tener cualquier error, tener en cuenta cuando utilizarlo
+  dynamic mensajeError = "Este es el mensaje error";
+  mensajeError = 1;
+  mensajeError = true;
+  mensajeError = [1,2,3,4,5,6];
+  mensajeError = {1,2,34,5,6,7,};
 
   
   print("""
@@ -13,6 +20,7 @@ void main (){
   $isAlive
   $abilities
   $sprites
+  $mensajeError
   """);
   
   
