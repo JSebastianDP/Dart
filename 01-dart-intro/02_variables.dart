d2661d6020c2c59eb115092a8904175247506fd7
JSebastianDP <sebastiandiaz28001@gmail.com>
2023-11-18 22:46:45 -0500

    Se comentarea el codigo

--- 01-dart-intro/02_variables.dart
+++ 01-dart-intro/02_variables.dart
@@ -8,12 +8,13 @@ void main (){
   
   //Tipo de variable "dynamic", es un tipo de variable que puede tener cualquier error, tener en cuenta cuando utilizarlo
   dynamic mensajeError = "Este es el mensaje error";
+  //Se definen algunos tipos del tipo dynamic
   mensajeError = 1;
   mensajeError = true;
   mensajeError = [1,2,3,4,5,6];
   mensajeError = {1,2,34,5,6,7,};
 
-  
+  //Se imprimen los diferentes tipos de variables en consola
   print("""
   $pokemon
   $vida
